cbcebbec44a6a4db71a2b8f698361a07bba7213c
junyaoren <renjunyao@gmail.com>
2019-06-11 19:20:01 -0700

    password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,11 +26,16 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new
+    @user.name = user_params[:name]
+
+    @user.password = user_params[:password_digest]
+
+    #@user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created. You can proceed to log in and manage the store" }
+        format.html { redirect_to login_path, notice: "User #{@user.name} was successfully created. You can proceed to log in and manage the store" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
